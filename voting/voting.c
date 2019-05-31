a2aca53da4b9d42d0dbe3f9cb637e3268c4aa425
Will Scott <will@oasislabs.com>
2019-05-31 12:39:20 -0700

    remove malloc

--- voting/voting.c
+++ voting/voting.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ADDRESS_LENGTH 20
+
 int closeVoting(FILE*);
 
 int main(int argc, char** argv) {
   char* home = getenv("HOME");
   size_t home_len = strlen(home);
   char* sender = getenv("SENDER");
-  size_t sender_len = strlen(sender);
-  char* owner_path = malloc(home_len + sender_len + 2);  // "/owner\0"
-  strncpy(owner_path, home, home_len);
-  strncpy(owner_path + home_len, "/owner\0", 7);
+  char path[64];  // "/home/<address>/owner"
+  strncpy(path, home, home_len);
+  strncpy(path + home_len, "/owner\0", 7);
 
-  FILE* fd = fopen(owner_path, "rb");
+  FILE* fd = fopen(path, "rb");
   if (fd == NULL) {
     // constructor.
 
     // set owner, state.
-    fd = fopen(owner_path, "wb");
+    fd = fopen(path, "wb");
     fprintf(fd, "1");
     fwrite(sender, strlen(sender), 1, fd);
     fclose(fd);
@@ -26,12 +27,10 @@ int main(int argc, char** argv) {
     // set candidates.
     int num_candidates;
     scanf("%d", &num_candidates);
-    char* candidate_path = malloc(home_len + 15);  // "/<number>\0"
-    strncpy(candidate_path, home, home_len);
-    candidate_path[home_len] = '/';
+    path[home_len] = '/';
     for (; num_candidates >= 0; num_candidates--) {
-      snprintf(candidate_path + home_len + 1, 14, "%d\0", num_candidates);
-      fd = fopen(candidate_path, "wb");
+      snprintf(path + home_len + 1, 14, "%d\0", num_candidates);
+      fd = fopen(path, "wb");
       fprintf(fd, "%d\0", 0);
       fclose(fd);
     }
@@ -57,15 +56,15 @@ int main(int argc, char** argv) {
   }
 
   // has this sender already voted?
-  strncpy(owner_path + home_len + 1, sender, sender_len);
-  fd = fopen(owner_path, "rb");
+  strncpy(path + home_len + 1, sender, ADDRESS_LENGTH);
+  fd = fopen(path, "rb");
   if (fd != NULL) {
     // already voted.
     return 1;
   }
 
   // cast a vote.
-  fd = fopen(owner_path, "wb");
+  fd = fopen(path, "wb");
   if (fd == NULL) {
     // write failed.
     return 2;
@@ -74,8 +73,8 @@ int main(int argc, char** argv) {
   fclose(fd);
 
   // increment the vote count for the candidate.
-  snprintf(owner_path + home_len + 1, 14, "%d\0", vote);
-  fd = fopen(owner_path, "rb");
+  snprintf(path + home_len + 1, 14, "%d\0", vote);
+  fd = fopen(path, "rb");
   if (fd == NULL) {
     // no such candidate.
     return 1;
@@ -85,7 +84,7 @@ int main(int argc, char** argv) {
 
   vote++;
 
-  fd = fopen(owner_path, "wb");
+  fd = fopen(path, "wb");
   fprintf(fd, "%d\0", vote);
   fclose(fd);
 
@@ -96,7 +95,7 @@ int closeVoting(FILE* fd) {
   char* home = getenv("HOME");
   size_t home_len = strlen(home);
   char* sender = getenv("SENDER");
-  char* candidate_path = malloc(home_len + 15);  // "/<number>\0"
+  char path[64];  // "/home/<address>/<number>\0"
 
   int vote;
   int idx;
@@ -113,11 +112,11 @@ int closeVoting(FILE* fd) {
 
   // find max vote
   vote = 0;
-  strncpy(candidate_path, home, home_len);
-  candidate_path[home_len] = '/';
+  strncpy(path, home, home_len);
+  path[home_len] = '/';
   while (1) {
-    snprintf(candidate_path + home_len + 1, 14, "%d\0", idx);
-    fd = fopen(candidate_path, "rb");
+    snprintf(path + home_len + 1, 14, "%d\0", idx);
+    fd = fopen(path, "rb");
     if (fd == NULL) {
       break;
     }
@@ -131,8 +130,8 @@ int closeVoting(FILE* fd) {
   }
 
   // update owner with winner.
-  strncpy(candidate_path + home_len, "/owner\0", 7);
-  fd = fopen(candidate_path, "wb");
+  strncpy(path + home_len, "/owner\0", 7);
+  fd = fopen(path, "wb");
   fprintf(fd, "0");
   fprintf(fd, "%d\0", max_idx);
   fclose(fd);
